37aa3c5ad7cdd2224c7170ce4fb86bff2af5971d
Brook Alemu <brookwworku3@gmail.com>
2023-12-04 10:30:31 -0900

    implementing_nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""N Queens placement on NxN chessboard"""
 
 
 import sys
 
 
-def generate_solutions(row, column):
-    solution = [[]]
+def generate_result(row, column):
+    res= [[]]
     for queen in range(row):
-        solution = place_queen(queen, column, solution)
-    return solution
+        res = place_queen(queen, column, res)
+    return res
 
 
 def place_queen(queen, column, prev_solution):
-    safe_position = []
+    position = []
     for array in prev_solution:
         for x in range(column):
             if is_safe(queen, x, array):
-                safe_position.append(array + [x])
-    return safe_position
+                position.append(array + [x])
+    return position
 
 
 def is_safe(q, x, array):
@@ -44,11 +43,11 @@ def init():
     return (n)
 
 
-def n_queens():
+def queens():
 
     n = init()
     # generate all solutions
-    solutions = generate_solutions(n, n)
+    solutions = generate_result(n, n)
     # print solutions
     for array in solutions:
         clean = []
@@ -58,4 +57,4 @@ def n_queens():
 
 
 if __name__ == '__main__':
-    n_queens()
+    queens()
