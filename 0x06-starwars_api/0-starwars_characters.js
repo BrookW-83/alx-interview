858cbb11a14bb3493f9c173b13dda1240e64b6a4
Brook Alemu <brookwworku3@gmail.com>
2023-12-19 06:59:39 -0900

    adding alx lockboxes project

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,31 +1,41 @@
 #!/usr/bin/node
+// script that prints all characters of a Star Wars movie
+// usage ./0-starwars_characters.js {movieID}
 
 const request = require('request');
 
 const movieId = process.argv[2];
-const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-function sendRequest (characterList, index) {
-  if (characterList.length === index) {
-    return;
-  }
+const filmsUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-  request(characterList[index], (error, response, body) => {
-    if (error) {
-      console.log(error);
+function printCharacters () {
+  request(filmsUrl, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const film = JSON.parse(body);
+      const characters = film.characters;
+
+      fetchCharacters(characters, 0);
     } else {
-      console.log(JSON.parse(body).name);
-      sendRequest(characterList, index + 1);
+      console.error('Failed to fetch movie data.');
     }
   });
 }
 
-request(movieEndpoint, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const characterList = JSON.parse(body).characters;
-
-    sendRequest(characterList, 0);
+function fetchCharacters (characterUrls, index) {
+  if (index >= characterUrls.length) {
+    return;
   }
-});
+
+  request(characterUrls[index], (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const character = JSON.parse(body);
+      console.log(character.name);
+
+      fetchCharacters(characterUrls, index + 1);
+    } else {
+      console.error('Failed to fetch character data.');
+    }
+  });
+}
+
+printCharacters();
